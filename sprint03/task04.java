5a23ec28990211668b0b75e165ecea70dfb048df
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-04-07 13:59:20 +0300

    Create task04.java

---
+++ sprint03/task04.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+public class MyUtils {
+    
+   public Map<String, List<String>> createNotebook(Map<String, String> phones) {
+
+        Map<String, List<String>> noteBook = new HashMap<>();
+
+        for (Map.Entry<String, String> entry : phones.entrySet()) {
+            
+            if (!noteBook.containsKey(entry.getValue())) {
+                noteBook.put(entry.getValue(), new ArrayList<>());
+            }
+            List<String> numbers = noteBook.get(entry.getValue());
+            numbers.add(entry.getKey());
+            noteBook.put(entry.getValue(), numbers);
+        }
+           return noteBook;
+   }
+
+}
