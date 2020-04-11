c1ecccaed0a1ad48762821c5897735a841cc27d3
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-04-11 13:10:42 +0300

    Create task03.java

---
+++ sprint04/task03.java
@@ -0,0 +1,16 @@
+public class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public void draw() {
+
+    }
+    public void draw(String color) {
+    }
+    public void draw(float scale) {
+    }
+    public void draw(String color,float scale) {
+    }
+}
