1de9c3c52aba1909fd8937a3e24d8ae8769956b3
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-04-13 12:14:47 +0300

    Create task04.java

---
+++ sprint05/task04.java
@@ -0,0 +1,33 @@
+abstract class Person {
+    protected int age;
+    protected String healthInfo;
+    protected String name;
+
+    public Person(int age, String healthInfo, String name) {
+        this.age = age;
+        this.healthInfo = healthInfo;
+        this.name = name;
+    }
+
+    String getHealthStatus() {
+        return name + " " + healthInfo;
+    }
+}
+
+class Child extends Person {
+    protected String childIDNumber;
+
+    public Child(int age, String healthInfo, String name, String childIDNumber) {
+        super(age, healthInfo, name);
+        this.childIDNumber = childIDNumber;
+    }
+}
+
+class Adult extends Person {
+    protected String passportNumber;
+
+    public Adult(int age, String healthInfo, String name, String passportNumber) {
+        super(age, healthInfo, name);
+        this.passportNumber = passportNumber;
+    }
+}
