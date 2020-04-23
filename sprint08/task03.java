087bb2e872026e440e39187217a14d61cce94da6
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-04-23 18:54:44 +0300

    Create task03.java

---
+++ sprint08/task03.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    //code
+     public static BinaryOperator<String> greetingOperator = (x, y) -> "Hello " + x + " " + y + "!!!";
+ public static List<String> createGreetings(List<Person> people, BinaryOperator<String> greetingOperator) {
+
+        return people.stream().flatMap(c-> Stream.of(greetingOperator.apply(c.name,c.surname))).collect(Collectors.toList());
+        //code
+    }
+}
