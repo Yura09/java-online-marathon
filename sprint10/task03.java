ddb69ff3e9aa90b450a567900b41e554c482d305
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-05-01 13:27:23 +0300

    Create task03.java

---
+++ sprint10/task03.java
@@ -0,0 +1,15 @@
+class Wrapper<T extends Shape> {
+    private T value;
+
+    public Wrapper(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
