cc1098cd5c16651efd805fd23f5c60e4394739e0
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-05-11 16:48:01 +0300

    Create task04.java

---
+++ sprint13/task04.java
@@ -0,0 +1,4 @@
+ public static String getDateAfterToday(int years, int months, int days) {
+        LocalDate date=LocalDate.now().plusYears(years).plusMonths(months).plusDays(days);
+        return date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
