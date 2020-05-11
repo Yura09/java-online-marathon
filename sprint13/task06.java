edee66616ff0b4af93f7822711117a8ef60a812a
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-05-11 19:11:47 +0300

    Create task06.java

---
+++ sprint13/task06.java
@@ -0,0 +1,15 @@
+public static void writeFile(String filename, String text) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char ch : text.toCharArray()) {
+
+            stringBuilder.append(("0000000" + Integer.toBinaryString(ch)).substring(Integer.toBinaryString(ch).length()));
+        }
+
+        try {
+            PrintWriter writer = new PrintWriter(filename, "UTF-8");
+            writer.print(stringBuilder.toString());
+            writer.close();
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
