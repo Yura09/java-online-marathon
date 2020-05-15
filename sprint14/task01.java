06539fd4a60df7a4fd18d71be0544977a34582e1
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-05-15 13:53:24 +0300

    Create task01.java

---
+++ sprint14/task01.java
@@ -0,0 +1,12 @@
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+public class MyUtils {
+public Stream<String> nameList(Map<String, Stream<String>> map) {
+        return map.values().stream().flatMap(obj -> obj).filter(Objects::nonNull).map(str -> str.replaceAll("\\s+", ""))
+                .filter(s -> !s.isEmpty())
+                .map(str -> str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase())
+                .distinct()
+                .sorted();
+    }
+}
