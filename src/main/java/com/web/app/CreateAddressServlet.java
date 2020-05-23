58c02218c5bd87c8586a1dcadb4e37024303fb94
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-05-23 15:00:39 +0300

    commit

---
+++ src/main/java/com/web/app/CreateAddressServlet.java
@@ -0,0 +1,38 @@
+package com.web.app;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addresses/create")
+public class CreateAddressServlet extends HttpServlet {
+    private AddressBook addressBook;
+
+    @Override
+    public void init() {
+        addressBook = AddressBook.getInstance();
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/create-address.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String address = request.getParameter("address");
+        if (!addressBook.create(firstName, lastName, address)) {
+            request.getSession().setAttribute("errorMessage", "An error occurred! Please, try again");
+            response.sendRedirect(request.getHeader("Referer"));
+        } else {
+            request.getSession().removeAttribute("errorMessage");
+            response.sendRedirect("/addresses/list");
+        }
+    }
+
+
+}
---
+++ target/AddressBook/error.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Exception</title>
+</head>
+<body>
+
+<h2><%= request.getAttribute("javax.servlet.error.message") %></h2>
+</body>
+</html>
