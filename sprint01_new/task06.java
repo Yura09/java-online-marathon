b03fb781af7ce0a4a1f06438b493688e6b7c7f20
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-06-11 11:38:46 +0300

    Create task06.java

---
+++ sprint01_new/task06.java
@@ -0,0 +1,35 @@
+class Product {
+    private static int createdObjects;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+        createdObjects++;
+    }
+
+    public Product() {
+        createdObjects++;
+    }
+
+    public static int count() {
+        return createdObjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
