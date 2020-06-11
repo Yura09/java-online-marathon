d69df4d97413c64df03289741f904f6d0623ed9f
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-06-11 11:27:45 +0300

    Create task03.java

---
+++ sprint01_new/task03.java
@@ -0,0 +1,28 @@
+public class Employee {
+    private String fullName;
+    private float salary;
+
+    public Employee(String fullName, float salary) {
+        this.fullName = fullName;
+        this.salary = salary;
+    }
+    public Employee(){
+        
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
