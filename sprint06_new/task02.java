eb28021cd1559b7b442951d8be6b500c7dfb3476
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-06-29 21:40:39 +0300

    Create task02.java

---
+++ sprint06_new/task02.java
@@ -0,0 +1,20 @@
+import java.util.function.Consumer;
+import java.util.Arrays;
+public class App {
+  public static Consumer<double[]> cons = array -> {
+       for (int i = 0; i < array.length; i++){
+           if(array[i] > 2.0){
+               array[i] *= 0.8;
+           }else {
+               array[i] *= 0.9;
+           }
+       }
+    };
+ 
+   public static double[] getChanged(double[] initialArray, Consumer<double[]> consumer) {
+        double[]array = initialArray.clone();
+        consumer.accept(array);
+
+    return array;
+    }
+}
