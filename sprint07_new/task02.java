b6a5fc5aacc15df63dcf37ff37d26733b298965c
Yura09 <43988243+Yura09@users.noreply.github.com>
2020-07-03 21:18:34 +0300

    Create task02.java

---
+++ sprint07_new/task02.java
@@ -0,0 +1,47 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface Review {
+    String reviewer();
+
+    String date() default "today";
+}
+
+class Util {
+    public static void review(String className) {
+        Class myClass;
+        try {
+            myClass = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Class " + className + " was not found");
+            return;
+        }
+        Review annotation = (Review) myClass.getAnnotation(Review.class);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date;
+        if (annotation.date().equals("today")) {
+            date = simpleDateFormat.format(new Date());
+        } else {
+            Date d = null;
+            try {
+                d = simpleDateFormat.parse(annotation.date());
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+            date = simpleDateFormat.format(d);
+        }
+        if (annotation.reviewer().isEmpty()) {
+            System.out.println("Class " + className + " isn't marked as Reviewed");
+        } else {
+            System.out.println("Class " + className + " was reviewed " + date + " by " + annotation.reviewer());
+        }
+    }
+
+}
